8f5894fff2889b761b83951b1bbe026118cb3549
Ivan <aiven715@gmail.com>
2021-09-04 19:14:40 +0300

    Update time, remove full weather, add noto emoji fonts

--- roles/dotfiles/files/.config/polybar/scripts/weather_full.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-print_weather() {
-  curl -s wttr.in/minsk?QFT
-}
-
-print_weather | dzen2 -p -l 36 -w 1376 -x 2200 -y 72 -fn 'Fantasque Sans Mono:size=11' -e 'onstart=hide,togglecollapse;button1=exit;button3=exit'
